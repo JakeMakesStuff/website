a7dbcd6b85eeea3dbfac7bcc9a739f759ae2ba08
Astrid Gealer <astrid@gealer.email>
2024-10-08 16:25:21 +0100

    prettier

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,15 +8,19 @@ export async function GET() {
         title: "Astrid's Blog",
         description: "A blog about my life and thoughts on tech",
         site: url,
-        items: await Promise.all(Object.entries(blogPosts).map(async ([path, postPromise]) => {
-            const post = await postPromise();
-            return {
-                title: post.frontmatter.title,
-                description: post.frontmatter.description,
-                pubDate: post.frontmatter.date,
-                link: path.replace(/\.md$/, "").replace(/^\.\/blog\//, "/blog/"),
-            }
-        })),
+        items: await Promise.all(
+            Object.entries(blogPosts).map(async ([path, postPromise]) => {
+                const post = await postPromise();
+                return {
+                    title: post.frontmatter.title,
+                    description: post.frontmatter.description,
+                    pubDate: post.frontmatter.date,
+                    link: path
+                        .replace(/\.md$/, "")
+                        .replace(/^\.\/blog\//, "/blog/"),
+                };
+            }),
+        ),
         customData: "<language>en-GB</language>",
     });
 }
