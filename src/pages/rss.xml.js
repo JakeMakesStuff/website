29cbb3a6fa418eea7d75bf1ead8649901a7789b2
Astrid Gealer <astrid@gealer.email>
2024-10-08 16:19:46 +0100

    change url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,5 @@
 import rss from "@astrojs/rss";
+import { url } from "../info";
 
 const blogPosts = import.meta.glob("./blog/*.md");
 
@@ -6,7 +7,7 @@ export async function GET(context) {
     return rss({
         title: "Astrid's Blog",
         description: "A blog about my life and thoughts on tech",
-        site: context.site,
+        site: url,
         items: await Promise.all(Object.entries(blogPosts).map(async ([path, postPromise]) => {
             const post = await postPromise();
             return {
