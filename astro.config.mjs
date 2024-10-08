e6df46c4d3a7c11ab8ae6e19e09299df838eca1b
Astrid Gealer <astrid@gealer.email>
2024-10-08 05:12:55 +0100

    switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
     output: "hybrid",
     integrations: [tailwind()],
-    adapter: cloudflare(),
+    adapter: vercelServerless(),
 });
