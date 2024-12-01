f342bb361c07f863f26cb8a522f11f97fdff8904
Astrid Gealer <astrid@gealer.email>
2024-12-01 17:52:34 +0000

    push BlueskyComments component to blog

--- src/scripts/blueskyCache.ts
+++ src/scripts/blueskyCache.ts
@@ -4,6 +4,9 @@ import type { ThreadViewPost } from "@atproto/api/dist/client/types/app/bsky/fee
 const threads: Map<string, Promise<ThreadViewPost>> = new Map();
 
 export async function getThread(uri: string) {
+    const r = threads.get(uri);
+    if (r) return r;
+
     const id = uri.split("/").pop()!;
     const thread = actions.getBlueskyPostById({ id }).then((r) => {
         if (r.error) throw r.error;
